f3788ee2020187f3f745bc9c4d456c06e102f866
soojjung <89066676+soojjung@users.noreply.github.com>
2024-08-15 23:14:11 +0900

    feat: reduce initial JS execution time

--- js/products.js
+++ js/products.js
@@ -60,9 +60,24 @@ function displayProducts(products) {
   });
 }
 
-loadProducts();
+// The onload event only occurs when the whole page is loaded.
+window.onload = () => {
+  let status = "idle";
+  let productSection = document.querySelector("#all-products");
 
-// Simulate heavy operation. It could be a complex price calculation.
-for (let i = 0; i < 10000000; i++) {
-  const temp = Math.sqrt(i) * Math.sqrt(i);
-}
+  window.onscroll = () => {
+    let position =
+      productSection.getBoundingClientRect().top -
+      (window.scrollY + window.innerHeight);
+
+    if (status == "idle" && position <= 0) {
+      status = "fetching";
+      loadProducts();
+
+      // Simulate heavy operation. It could be a complex price calculation.
+      for (let i = 0; i < 10000000; i++) {
+        const temp = Math.sqrt(i) * Math.sqrt(i);
+      }
+    }
+  };
+};
